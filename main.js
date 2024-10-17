3707653a1a03da4259fc97f4e2736019581c7b2c
Jonas Freire <jonasribeiro235@gmail.com>
2024-10-17 07:11:35 -0300

    aplique um alerta(uma condição pra lista.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function addNewTask() {
     var list = document.getElementById("list");
     var text = document.getElementById("task_name").value;
+   
+    if(text.length === 0){
+        alert("tarefa precisa ter mais de 1 caractere");
+        return;
+    }
+
     var listItem = document.createElement("li");
     listItem.className = "list-item"
     
